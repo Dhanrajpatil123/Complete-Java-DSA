2f07645705594a04eb48b7e53fb6d45972aab776
Dhanraj Patil <95074320+Dhanrajpatil123@users.noreply.github.com>
2024-12-10 22:00:23 +0530

    Create 2DArrayInsert.java

---
+++ 16-Array/2DArrayInsert.java
@@ -0,0 +1,51 @@
+package com.day_2;
+
+import java.util.Scanner;
+
+public class MyArray {
+
+    public static void print2DArray(int[][] array){
+        for (int[] x : array){
+            for (int y : x)
+                System.out.print(y + " ");
+            System.out.println();
+        }
+    }
+
+    public static int[][] insertElement2D(int[][] array, int position1, int position2, int element){
+
+        if (array.length <= 0 || position1 > array.length){
+            System.out.println("Something went wrong");
+        }
+
+        for (int row = 0; row < array.length; row++) {
+            for (int col = 0; col < array[row].length; col++) {
+                if (row == position1 && col == position2){
+                    array[position1][position2] = element;
+                    break;
+                }
+            }
+        }
+
+        return array;
+    }
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        int[][] array = new int[3][3];
+
+        System.out.println("Enter Array Elements : ");
+        for (int i = 0; i < array.length; i++) {
+            for (int j = 0; j < array[i].length; j++) {
+                array[i][j] = sc.nextInt();
+            }
+        }
+
+        print2DArray(array);
+        System.out.println("----------");
+        print2DArray(insertElement2D(array, 1, 2, 100));
+
+    }
+}
