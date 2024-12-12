fcd56e8b8aa2a2ff97b70895db7d9131ae08d0f0
Dhanraj Patil <95074320+Dhanrajpatil123@users.noreply.github.com>
2024-12-12 18:33:56 +0530

    Problem - 3 ( Solution )

---
+++ 19- Interview Question/Problem - 3 ( Solution ).java
@@ -0,0 +1,38 @@
+package com.day_2;
+
+import java.util.Arrays;
+
+public class _19_Interview_Que {
+
+    public static int binarySearchFloor(int[] arr, int target){
+
+        int start = 0;
+        int end = arr.length-1;
+        int ans = -1;
+
+        while (start <= end){
+
+            int mid = start + (end - start) /2 ;
+
+            if (arr[mid] == target){
+                return ans;
+            }
+            else if (arr[mid] < target){
+                start = mid + 1;
+                ans = arr[mid];
+            }
+            else {
+                end = mid - 1;
+            }
+        }
+        return ans;
+    }
+
+    public static void main(String[] args) {
+
+        int[] arr = {4, 4, 8, 8, 8, 15, 16, 23, 23, 42};
+
+        System.out.println("First & Last Occurrence : " + binarySearchFloor(arr, 10));
+
+    }
+}
